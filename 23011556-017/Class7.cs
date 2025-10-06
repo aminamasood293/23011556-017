df199b4624d6e5c9a138883c5c286d3c4caa6736
aminamasood293 <aminamasood293@gmail.com>
2025-10-06 09:50:41 -0700

    rev

---
+++ 23011556-017/Class7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rev
+{
+    internal class Class7
+    {
+        public void reverse()
+        {
+            Console.WriteLine("enter a Number ");
+         int num = Convert.ToInt32(Console.ReadLine());
+        int n = num;
+        int rev = 0;
+        int digit;
+         while (num != 0)
+         {
+             digit = num % 10;
+             rev = (rev* 10) + digit;
+             num=num/ 10;
+         }
+    Console.WriteLine("the number is " + n +" and the reverse is " +rev);
+     }
+    }
+}
