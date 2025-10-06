49ae5d9c1d6be89215bbe59f27490c1bed816dab
aminamasood293 <aminamasood293@gmail.com>
2025-10-06 09:54:12 -0700

    maxmin

---
+++ 23011556-017/Class8.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace max_min
+{
+    internal class Program
+    {
+        public void maxmin()
+        {
+            //static void Main(string[] args)
+            //{
+            Console.WriteLine("enter 10 Numbers ");
+
+            int[] arr = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int max = arr[0];
+            for (int i = 0; i < 10; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            Console.WriteLine("max is " + max);
+
+            int min = arr[0];
+            for (int i = 0; i < 10; i++)
+            {
+                if (arr[i] < min)
+
+                {
+                    min = arr[i];
+
+                }
+            }
+            Console.WriteLine("min is " + min);
+        }
+        //}
+    }
+}
