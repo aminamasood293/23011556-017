8f07cba6c2493c821d290ca7b4d73d0f0aa4638a
aminamasood293 <aminamasood293@gmail.com>
2025-10-06 09:49:01 -0700

    fact

---
+++ 23011556-017/Class6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fact
+{
+    
+    internal class Class6
+    {
+        public void factorial()
+        {
+            Console.WriteLine("enter a Number ");
+         int num = Convert.ToInt32(Console.ReadLine());
+        int fact = 1;
+         if (num == 0 || num == 1)
+         {
+             Console.WriteLine("factorial is 1 ");
+         }
+         else
+         {
+             int i = 1;
+             while (i <= num)
+             {
+                 fact = fact* i;
+    i++;
+             }
+Console.WriteLine("The factorial is:" + fact);
+             //Console.WriteLine($"factorial is {fact}");
+         }
+}}}
