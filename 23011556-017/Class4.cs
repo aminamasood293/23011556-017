bd25fa19db96fb2859264dc53aad1f1d170571a4
aminamasood293 <aminamasood293@gmail.com>
2025-10-06 09:41:14 -0700

    sum

---
+++ 23011556-017/Class4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum
+{
+    internal class Class4
+    {
+        public void series_sum()
+        {
+            //static void Main(string[] args)
+            //{
+            int sum = 0;
+            Console.WriteLine("enter a Number ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+
+                sum = sum + i;
+
+            }
+            Console.WriteLine($"The sum is:{sum} ");
+        }
+        }
+   // }
+}
