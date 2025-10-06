d3e336d2ad375d27879228f50c0555f04ed48a62
aminamasood293 <aminamasood293@gmail.com>
2025-10-06 09:44:50 -0700

    table

---
+++ 23011556-017/Class5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace table
+{
+    internal class Class5
+    {
+        public void table()
+        {
+            Console.WriteLine("enter a Number ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                //Console.WriteLine($"{num} * {i} = {num*i}");
+                Console.WriteLine(num + " x " + i + "  =  " + num * i);
+            }
+
+        }
+    }
+}
