be98a191b92d4609be0cee43f1bcc677e8d844b1
aminamasood293 <aminamasood293@gmail.com>
2025-10-06 09:33:52 -0700

    grade

---
+++ 23011556-017/Class3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grade
+{
+    internal class Class3
+    {
+        public void grades()
+        {
+            //static void Main(string[] args)
+            //{
+            Console.WriteLine("enter your marks ");
+            int grades = Convert.ToInt32(Console.ReadLine());
+            if (grades >= 85)
+            {
+                Console.WriteLine("your grade is 'A' ");
+            }
+            else if (grades >= 70 && grades <= 84)
+            {
+                Console.WriteLine("your grade is 'B' ");
+            }
+            else if (grades >= 55 && grades <= 69)
+            {
+                Console.WriteLine("your grade is 'C' ");
+            }
+            else if (grades >= 40 && grades <= 54)
+            {
+                Console.WriteLine("your grade is 'D' ");
+            }
+            else if (grades < 40)
+            {
+                Console.WriteLine("your grade is 'F' ");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input ");
+            }
+        }
+    }
+}
